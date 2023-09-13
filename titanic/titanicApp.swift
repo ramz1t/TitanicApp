f37b86d997c1b46692106860158c851590f06db4
Timur Ramazanov <timurram007@gmail.com>
2023-09-13 12:55:45 +0200

    Initial Commit

---
+++ titanic/titanicApp.swift
@@ -0,0 +1,17 @@
+//
+//  titanicApp.swift
+//  titanic
+//
+//  Created by Timur Ramazanov on 13.09.2023.
+//
+
+import SwiftUI
+
+@main
+struct titanicApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
