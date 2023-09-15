989616f7e90eec2c06d38d04341ce6b69219158b
Timur Ramazanov <timurram007@gmail.com>
2023-09-15 14:38:14 +0200

    fixes, about tab

--- titanic/ContentView.swift
+++ titanic/ContentView.swift
@@ -14,23 +14,27 @@ struct ContentView: View {
     @State private var sibsp = 0
     @State private var parch = 0
     @State private var sex = "male"
-    @State private var embarked = ""
-    @State private var prediction = "S"
+    @State private var embarked = "S"
+    @State private var prediction = ""
     @State private var isAlert = false
-    let predictionOptions = ["😔 You're dead", "YAY! You survived"]
+    @State private var isInfoAlert = false
+    let predictionOptions = ["You died", "You survived"]
     
     func makePrediction() -> Void {
         do {
             let config = MLModelConfiguration()
             let model = try TitanicClassifier(configuration: config)
-            let mPred = try model.prediction(Pclass: Int64(pclass),
+            
+            let modelPred = try model.prediction(Pclass: Int64(pclass),
                                              Sex: sex,
                                              Age: Double(age),
                                              SibSp: Int64(sibsp),
                                              Parch: Int64(parch),
                                              Embarked: embarked)
-            let predLabel = predictionOptions[Int(mPred.Survived)]
-            let percent = (mPred.SurvivedProbability[mPred.Survived] ?? 0) * 100
+            
+            let predClass = modelPred.Survived
+            let predLabel = predictionOptions[Int(predClass)]
+            let percent = (modelPred.SurvivedProbability[predClass] ?? 0) * 100
             
             prediction = predLabel + " with \(Int(percent))% chance"
         } catch {
@@ -54,6 +58,7 @@ struct ContentView: View {
                 } footer: {
                     Text("A proxy for socio-economic status (SES)\n1st = Upper\n2nd = Middle\n3rd = Lower")
                 }
+                
                 Section {
                     Picker("sex", selection: $sex) {
                         Text("Male")
@@ -62,21 +67,25 @@ struct ContentView: View {
                             .tag("female")
                     }
                 }
+                
                 Section {
                     Picker("age", selection: $age) {
                         ForEach(1..<100) { Text("\($0) years").tag($0) }
                     }
                 }
+                
                 Section {
                     Stepper("sibsp \(sibsp)", value: $sibsp, in: 0...100)
                 } footer: {
                     Text("# of siblings / spouses aboard the Titanic")
                 }
+                
                 Section {
                     Stepper("parch \(parch)", value: $parch, in: 0...100)
                 } footer: {
                     Text("# of parents / children aboard the Titanic")
                 }
+                
                 Section {
                     Picker("embarked", selection: $embarked) {
                         Text("Cherbourg")
@@ -92,12 +101,26 @@ struct ContentView: View {
             }
             .navigationTitle("Survival on Titanic")
             .toolbar {
-                Button("Predict") { makePrediction() }
+                ToolbarItem(placement: .primaryAction) {
+                    Button("Predict") { makePrediction() }
+                }
+                ToolbarItem(placement: .cancellationAction) {
+                    Button {
+                        isInfoAlert = true
+                    } label: {
+                        Label("Info", systemImage: "info.circle")
+                    }
+                }
             }
             .alert("Your prediction", isPresented: $isAlert) {
                 Button("OK") {}
             } message: {
                 Text(prediction)
+            }
+            .alert("About", isPresented: $isInfoAlert) {
+                Button("Close") {}
+            } message: {
+                Text("This app is created using SwiftUI and CoreML\nAuthor: github.com/ramz1t")
             }
         }
     }
